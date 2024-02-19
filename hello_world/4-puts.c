45aa5c4a8bea1278b8ed8430e6bc4a344408062e
Abdelaziz5959 <abdelaziz.amharssi@gmail.com>
2024-02-19 14:44:29 +0100

    program that prints exactly with followed by line

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,14 @@
+#include <stdio.h>
 
-int main()
+/**
+ * main - program that prints exactly
+ *
+ * Return: Always 0
+ */
+
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-return 0;
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
+
