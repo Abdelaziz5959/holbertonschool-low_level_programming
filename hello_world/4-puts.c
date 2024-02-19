92c47e6ed3a0058b39346cd25c1f99ed4ee2e54a
Abdelaziz5959 <abdelaziz.amharssi@gmail.com>
2024-02-19 14:29:53 +0100

    Write a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #!bin/bash
-put main()
+int main()
 {
-	scanf"Programming is like building a multilingual puzzle\n);
-return=0;
+puts("Programming is like building a multilingual puzzle\n");
+return 0;
 }
