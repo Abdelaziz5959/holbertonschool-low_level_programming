7bd92e4845a7a54f451ac9639d6c095352d160e3
Abdelaziz5959 <abdelaziz.amharssi@gmail.com>
2024-02-21 09:18:37 +0100

    1-last_digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,9 +6,7 @@
 * main - The last digit
 *
 * Description: This program will assign a random number to
-* the variable n each time it is executed. Complete the source
-* code in order to print the last digit of the number stored
-* in the variable n.
+* the variable n each time it is executed.
 *
 * Return: Always (0)
 */
